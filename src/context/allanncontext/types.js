35af811aad4933d4aa1440930984461f68129f7b
kost90 <kostdonc90@gmail.com>
2024-01-09 14:48:44 +0000

    edited utils for map page

--- src/context/allanncontext/types.js
+++ src/context/allanncontext/types.js
@@ -1,3 +1,4 @@
 export const ANNOUNCE_ACTION_TYPES = {
     addAnnounce: "ADD_ANNOUNCE",
+    changeAnnounce:"CHANGE_ANNOUNCE",
   };
--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -2,10 +2,14 @@ import React, { useRef, useEffect, useState } from "react";
 import styles from "./Map.module.css";
 import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
-import { useAnnouncement } from "../../context/allanncontext/Anouncement.jsx";
+import {
+  useAnnouncement,
+  useDispatch,
+} from "../../context/allanncontext/Anouncement.jsx";
 import { useSingleDispatch } from "../../context/singleannounc/Singleannounce";
 import { SINGLANN_ACTION_TYPES } from "../../context/singleannounc/types.js";
-import { DrawInitMap, clickHandler } from "./utils.jsx";
+import { DrawInitMap, FilterCardList, clickHandler } from "./utils.jsx";
+// import { ANNOUNCE_ACTION_TYPES } from "../../context/allanncontext/types.js";
 
 const API_key = import.meta.env.VITE_API_KEY;
 
@@ -17,9 +21,11 @@ function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const kiev = { lng: 30.523, lat: 50.45 };
-  const [zoom] = useState(5);
+  const [zoom,setZoom] = useState(5);
   const disptach = useSingleDispatch();
-
+  const dispatchAll = useDispatch();
+  const [scrollZoom,setScrollZoom] = useState(null);
+ 
   useEffect(() => {
     if (map.current) return;
     map.current = new maptilersdk.Map({
@@ -32,8 +38,10 @@ function Map() {
 
   useEffect(() => {
     if (map.current.loaded()) {
+
       const newArr = [...contextData];
       const lastElem = newArr.slice(-1);
+
       DrawInitMap(map, lastElem[0].id, lastElem[0].geo, contextData);
       map.current.on("click", "unclustered-point", function (e) {
         const id = e.features[0].id;
@@ -49,8 +57,26 @@ function Map() {
         const id = e.features[0].id;
         setClickedID(id);
       });
+      
+      // map.current.on("zoom", function () {
+      //   setScrollZoom(map.current.getZoom());
+      //   console.log(scrollZoom)
+      //   const clusters = map.current.queryRenderedFeatures({
+      //     layers: ["unclustered-point"],
+      //   });
+      //   if(scrollZoom >= 10){
+      //     console.log('zoom avaieble')
+      //       const newShowArr = FilterCardList(clusters, contextData);
+      //       dispatchAll({
+      //         type: ANNOUNCE_ACTION_TYPES.changeAnnounce,
+      //         payload: newShowArr,
+      //       });
+      //   }else{
+      //     return
+      //   }
+      // });
     }
-  }, [contextData]);
+  }, [contextData,scrollZoom]);
 
   useEffect(() => {
     if (clikeId !== null) {
